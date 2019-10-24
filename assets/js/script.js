48c158d3d237b82566d3721dec3963f560b8d615
LILIE6630 <lili6630.al@gmail.com>
2019-10-24 10:59:30 +0200

    création ex2

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+function show(){
+  ///Pour afficher la division :
+  document.getElementById('text').style.display = "block";
+}
+function hide(){
+  //Pour masquer la division :
+  document.getElementById('text').style.display = "none";
+}
