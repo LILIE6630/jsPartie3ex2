f7de432604990a5735f68bbcf5243f3d29d9a933
LILIE6630 <lili6630.al@gmail.com>
2019-10-24 16:07:04 +0200

    correction ex2

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,8 @@
 function show(){
   ///Pour afficher la division :
-  document.getElementById('text').style.display = "block";
+  document.getElementById('text').style.display = 'block';
 }
 function hide(){
   //Pour masquer la division :
-  document.getElementById('text').style.display = "none";
+  document.getElementById('text').style.display = 'none';
 }
